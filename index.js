aee30806d3abbcb64a094a596238da4182a8a5ab
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-15 13:17:58 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function mapToNegativize(arr){
+    return arr.map(x => x * (-1));
+}
+
+function mapToNoChange(arr){
+    return arr.map(x => x);
+}
+
+function mapToDouble(arr){
+    return arr.map(x => x * 2);
+}
+
+function mapToSquare(arr){
+    return arr.map(x => x**2);
+}
